dc7793ed00c1fe37de659995d86062d2dbc5f191
Joakim5005 <fherach@gmail.com>
2023-02-19 09:49:03 +0100

    Move writeups to another repo

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 mkdir ./docs
 
 #Declare a string array
-FileArray=("index.html" "logo.png" "logo_long.png" "key.pub" "CNAME" "googled49ee5ba9d18fa74.html" "wallet_1.png" "wallet_2.png" "et-nul.png" "writeups.html")
+FileArray=("index.html" "logo.png" "logo_long.png" "key.pub" "CNAME" "googled49ee5ba9d18fa74.html")
  
 for file in ${FileArray[*]}; do
      cp $file ./docs/
